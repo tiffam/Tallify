6707aab8bcfece68b249d963235d5c76b5f99ac6
Ticktock <smu@ticktock.local>
2018-04-08 20:40:58 +0800

    added info for heroku set-up

--- db.js
+++ db.js
@@ -4,6 +4,25 @@ const users = require('./models/users');
 const company = require('./models/company');
 const vouchers = require('./models/vouchers');
 
+const url = require ('url');
+
+if( process.env.DATABASE_URL ){
+	const params = url.parse(process.env.DATABASE_URL);
+  	const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+  }else{
+
+
 const configs = {
   user: 'smu',
   host: '127.0.0.1',
