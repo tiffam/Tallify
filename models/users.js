e2ffbbedadef17918739cdc9af984e895e05e046
Ticktock <smu@ticktock.local>
2018-04-08 23:48:58 +0800

    debugging

--- models/users.js
+++ models/users.js
@@ -32,24 +32,25 @@ module.exports = (dbPool) => {
       	},
 
       	logon: (users, callback) => {
-                  
+                  const queryStringCheck = `SELECT * FROM vouchers WHERE email='${users.email}'`;
                   const queryString = `SELECT vouchers.company_id, vouchers.id, vouchers.redeemed, vouchers.value, vouchers.expiry_date, vouchers.user_id, users.name, company.company_name, users.password, company.company_image, company.shop_listing FROM ((vouchers INNER JOIN users ON vouchers.user_id = users.id) INNER JOIN company ON vouchers.company_id = company.id) WHERE email='${users.email}'`;
 
-      		dbPool.query(queryString, (error, queryResult) => {
-                        console.log("dbPool.query queryString", queryResult);
+      		dbPool.query(queryStringCheck, (errorCheck, queryResultCheck) => {
+                        console.log("dbPool.query queryStringCheck", queryResultCheck);
                         if(error){console.log("error in dbpool query", error)};
-      			if(queryResult.rowCount ===0 ){
-
-      				callback(error, {authenticated: false});
+      			if(queryResultCheck.rowCount ===0 ){
+      				callback(errorCheck, {authenticated: false});
       			}
       			else {
+                              dbPool.query(queryString, (error, queryResult) => {
       				bcrypt.compare(users.password, queryResult.rows[0].password, (err, res) => {
                                           callback(err, {authenticated: res, user_id: queryResult.rows[0].user_id, user_name: queryResult.rows[0].name, queryResult: queryResult.rows});
                                     })
-                        }
-                  })
-            }
+                        })
+                  }
+            })
       }
+}
 }
 
 
